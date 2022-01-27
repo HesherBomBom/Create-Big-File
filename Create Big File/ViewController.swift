fe0e5e9d913cfcc7598ff7e3ab3432397a8cb08b
pzykov <zpavel411@gmail.com>
2022-01-27 22:42:10 +0300

    Initial Commit

---
+++ Create Big File/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Create Big File
+//
+//  Created by Павел Зыков on 27.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
